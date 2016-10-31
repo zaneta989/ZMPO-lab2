28c5a3e442dc0fabb44b49e11a94efde969d87ca
Żaneta Larwa <zaneta989@gmail.com>
2016-10-31 19:10:19 +0100

    Initial Commit

---
+++ lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab2
+//
+//  Created by Żaneta Larwa on 31.10.2016.
+//  Copyright © 2016 Żaneta Larwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
